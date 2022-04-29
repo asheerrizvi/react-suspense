9f9f878cdd0afccb8da3c8c45ecba3be2b761a05
Asheer Rizvi <asheer.vil@gmail.com>
2022-04-29 15:44:08 +0530

    Exercise 07: Coordinate Suspending Components with SuspenseList.

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -71,20 +71,27 @@ function App() {
           onReset={handleReset}
           resetKeys={[pokemonResource]}
         >
-          <React.Suspense fallback={fallback}>
-            <NavBar pokemonResource={pokemonResource} />
-          </React.Suspense>
-          <div className={cn.mainContentArea}>
+          <React.SuspenseList revealOrder="forwards" tail="collapsed">
             <React.Suspense fallback={fallback}>
-              <LeftNav />
+              <NavBar pokemonResource={pokemonResource} />
             </React.Suspense>
-            <React.Suspense fallback={fallback}>
-              <MainContent pokemonResource={pokemonResource} />
-            </React.Suspense>
-            <React.Suspense fallback={fallback}>
-              <RightNav pokemonResource={pokemonResource} />
-            </React.Suspense>
-          </div>
+            <div className={cn.mainContentArea}>
+              <React.SuspenseList revealOrder="forwards" tail="collapsed">
+                <React.Suspense fallback={fallback}>
+                  <LeftNav />
+                </React.Suspense>
+
+                <React.SuspenseList revealOrder="together">
+                  <React.Suspense fallback={fallback}>
+                    <MainContent pokemonResource={pokemonResource} />
+                  </React.Suspense>
+                  <React.Suspense fallback={fallback}>
+                    <RightNav pokemonResource={pokemonResource} />
+                  </React.Suspense>
+                </React.SuspenseList>
+              </React.SuspenseList>
+            </div>
+          </React.SuspenseList>
         </PokemonErrorBoundary>
       </div>
     </div>
